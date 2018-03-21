2417bcfdefe0aa499847b1095f6537393a97a4da
Sam Willis <samueljohnwillis@gmail.com>
2018-03-21 18:31:27 +0000

    Finished

--- js/game-logic.js
+++ js/game-logic.js
@@ -151,3 +151,59 @@ function getMoveWinner(playerOneMoveType, playerOneMoveValue, playerTwoMoveType,
 		}
 	}
 }
+
+function addWin(winner) {
+  if (winner === 'Player One') {
+    playerOneWins = (playerOneWins + 1);
+  } else if (winner === 'Player Two') {
+    playerTwoWins = (playerTwoWins + 1);
+  }
+}
+
+function getGameWinner() {
+	if (!playerOneMoveOneType ||
+		!playerOneMoveOneValue ||
+		!playerTwoMoveOneType ||
+		!playerTwoMoveOneValue ||
+		!playerOneMoveTwoType ||
+		!playerOneMoveTwoValue ||
+		!playerTwoMoveTwoType ||
+		!playerTwoMoveTwoValue ||
+		!playerOneMoveThreeType ||
+		!playerOneMoveThreeValue ||
+		!playerTwoMoveThreeType ||
+		!playerTwoMoveThreeValue) {
+		return null;
+	}
+
+  playerOneWins = 0;
+  playerTwoWins = 0;
+
+  const roundOneWinner = getRoundWinner(1);
+  const roundTwoWinner = getRoundWinner(2);
+  const roundThreeWinner = getRoundWinner(3);
+
+  addWin(roundOneWinner);
+  addWin(roundTwoWinner);
+  addWin(roundThreeWinner);
+
+  if (playerOneWins > playerTwoWins) {
+    return 'Player One';
+  } else if (playerOneWins < playerTwoWins) {
+    return 'Player Two';
+  } else {
+    return 'Tie';
+  }
+}
+
+function setComputerMoves() {
+  const moves = ['rock', 'paper', 'scissors'];
+  const moveOneType = moves[Math.floor(Math.random() * 3)];
+  const moveTwoType = moves[Math.floor(Math.random() * 3)];
+  const moveThreeType = moves[Math.floor(Math.random() * 3)];
+  const moveOneValue = Math.floor(Math.random() * 96) + 1;
+  const moveTwoValue = Math.floor(Math.random() * (97 - moveOneValue)) + 1;
+  const moveThreeValue = 99 - moveOneValue - moveTwoValue;
+  setPlayerMoves('Player Two', moveOneType, moveOneValue, moveTwoType,
+                 moveTwoValue, moveThreeType, moveThreeValue);
+}
